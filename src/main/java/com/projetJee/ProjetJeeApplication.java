4569d17c1c464e4e3acdfea3bdc5a14b5b9ff1f6
Mickaël Lafages <mickx971.l@gmail.com>
2017-04-05 14:12:41 +0200

    project creation

---
+++ src/main/java/com/projetJee/ProjetJeeApplication.java
@@ -0,0 +1,12 @@
+package com.projetJee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetJeeApplication.class, args);
+	}
+}
